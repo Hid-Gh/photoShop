e1551735b4ff4c31917f57cbeafc96c9ec132743
Hidaya-Gh <hidaya.gharad.2002@gmail.com>
2023-06-09 11:47:00 +0300

    done with the frontend of my opencv project

---
+++ Photoshop/main.js
@@ -0,0 +1,51 @@
+function openCvReady(){
+            cv["onRuntimeInitialized"]=()=>{
+                       
+                       
+                        let imgMain = cv.imread("img-main");
+                        
+                        cv.imshow("main-canvas",imgMain);
+                        imgMain.delete()
+                        document.getElementById("button-rgb").onclick = function(){
+                                    let imgMain = cv.imread("img-main");
+                                    cv.imshow("main-canvas",imgMain);
+                                    imgMain.delete()
+                        };
+                        document.getElementById("button-gray").onclick = function(){
+                                    let imgMain = cv.imread("img-main");
+                                    let imgGray = imgMain.clone();
+                                    cv.cvtColor(imgMain,imgGray,cv.COLOR_RGBA2GRAY,0)
+                                    cv.imshow("main-canvas",imgGray);
+                                    imgMain.delete()
+                                    imgGray.delete()
+
+                        };
+                        document.getElementById("button-blur").onclick = function(){
+                                    let imgMain = cv.imread("img-main");
+                                    let imgBlur = imgMain.clone();
+                                    let ksize= new cv.Size(49,49)
+                                    cv.GaussianBlur(imgMain,imgBlur,ksize,0)
+                        
+                                    cv.imshow("main-canvas",imgBlur)
+                                    imgBlur.delete()
+                                    imgMain.delete()
+
+                        };
+                        document.getElementById("button-edge").onclick = function(){
+                                    let imgMain = cv.imread("img-main");
+                                    let imgCanny = imgMain.clone();
+                                   
+                                   
+                                    cv.Canny(imgMain,imgCanny,50,100)
+                                    cv.imshow("main-canvas",imgCanny)
+                                    imgCanny.delete()
+                                    imgMain.delete()
+
+                        };
+                      
+                        imgGray.delete()
+                        imgBlur.delete()
+                        imgCanny.delete()
+            }
+
+}
